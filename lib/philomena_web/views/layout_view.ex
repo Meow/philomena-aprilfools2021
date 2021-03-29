6255e03b5aade234694a45d10bb1751d73ba03f0
Luna D <cod7777@yandex.ru>
2021-03-29 17:44:33 +0200

    preenhub initial changes

--- lib/philomena_web/views/layout_view.ex
+++ lib/philomena_web/views/layout_view.ex
@@ -64,20 +64,20 @@ defmodule PhilomenaWeb.LayoutView do
     Config.get(:footer)
   end
 
-  def stylesheet_path(conn, %{theme: "dark"}),
-    do: Routes.static_path(conn, "/css/dark.css")
+  # def stylesheet_path(conn, %{theme: "dark"}),
+  #   do: Routes.static_path(conn, "/css/dark.css")
 
-  def stylesheet_path(conn, %{theme: "red"}),
-    do: Routes.static_path(conn, "/css/red.css")
+  # def stylesheet_path(conn, %{theme: "red"}),
+  #   do: Routes.static_path(conn, "/css/red.css")
 
   def stylesheet_path(conn, _user),
-    do: Routes.static_path(conn, "/css/default.css")
+    do: Routes.static_path(conn, "/css/dark.css")
 
   def dark_stylesheet_path(conn),
     do: Routes.static_path(conn, "/css/dark.css")
 
   def theme_name(%{theme: theme}), do: theme
-  def theme_name(_user), do: "default"
+  def theme_name(_user), do: "dark"
 
   def artist_tags(tags),
     do: Enum.filter(tags, &(&1.namespace == "artist"))
